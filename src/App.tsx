ecf0e3e90318d98918e2fccbd797347d993e5b0b
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-09-17 14:53:07 -0300

    feat: header and transactions table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from 'styled-components'
-import './App.css'
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Transactions } from './pages/Transactions'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <GlobalStyle />
+
+      <Transactions />
     </ThemeProvider>
   )
 }
